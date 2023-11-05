23cfd0688a30d3b0501dd0c43b1d8cad39dd27d2
victorbtdias <victorbtdias159@hotmail.com>
2023-11-05 14:14:35 -0300

    Criação form addProduct

--- src/screens/AddProduct/index.tsx
+++ src/screens/AddProduct/index.tsx
@@ -1,12 +1,72 @@
 import { Text } from "react-native";
-import React from "react";
-import { Container } from "./styled";
+import React, { useState } from "react";
+import {
+  Container,
+  DescriptionContainer,
+  Division,
+  Input,
+  InputContainer,
+} from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
+import DropDownComponent from "../../components/common/DropDownComponent";
+import DefaultButton from "../../components/common/DefaultButton";
+
+const Address = [
+  { value: "Endereço 1" },
+  { value: "Endereço 2" },
+  { value: "Endereço 3" },
+  { value: "Endereço 4" },
+];
+
+const Categorie = [
+  { value: "Categoria 1" },
+  { value: "Categoria 2" },
+  { value: "Categoria 3" },
+  { value: "Categoria 4" },
+];
 
 const AddProduct = () => {
+  const [categorie, setCategorie] = useState("");
+  const [address, setAddress] = useState("");
+
   return (
     <Container>
       <DefaultTitle title="CADASTRO DO ANÚNCIO" fontSize={18} />
+      <InputContainer>
+        <Input placeholder="Título" />
+      </InputContainer>
+      <InputContainer>
+        <Input placeholder="Preço" keyboardType="numeric" />
+      </InputContainer>
+      <DescriptionContainer>
+        <Input placeholder="Descrição" />
+      </DescriptionContainer>
+      <InputContainer>
+        <Input placeholder="IMAGEM (SIMBÓLICO)" />
+      </InputContainer>
+      <DropDownComponent
+        data={Categorie}
+        placeholder="Selecione a categoria"
+        setSelected={setCategorie}
+      />
+      <DropDownComponent
+        data={Address}
+        placeholder="Selecione o endereço"
+        setSelected={setAddress}
+      />
+      <DefaultButton
+        buttonText="CADASTRAR E PUBLICAR"
+        buttonHandle={() => {}}
+        buttonType="primary"
+        marginVertical={20}
+      />
+      <Division>ou</Division>
+      <DefaultButton
+        buttonText="SALVAR COMO RASCUNHO"
+        buttonHandle={() => {}}
+        buttonType="secondary"
+        marginVertical={20}
+      />
     </Container>
   );
 };
