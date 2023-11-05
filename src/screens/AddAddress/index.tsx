8c70322048dfc29d3fffa572394b3d702c2ecd28
victorbtdias <victorbtdias159@hotmail.com>
2023-11-05 14:00:08 -0300

    Criação form addAddress

--- src/screens/AddAddress/index.tsx
+++ src/screens/AddAddress/index.tsx
@@ -1,11 +1,119 @@
-import React from "react";
-import { Container } from "./styled";
+import React, { useState } from "react";
+import { Container, Input, InputContainer, InputMask } from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
+import DefaultButton from "../../components/common/DefaultButton";
+import axios from "axios";
 
 const AddAddress = () => {
+  const [fields, setFields] = useState({
+    cep: "",
+    street: "",
+    number: "",
+    complement: "",
+    district: "",
+    city: "",
+    state: "",
+  });
+
+  const handleGetAddress = async () => {
+    const { data } = await axios.get(
+      `https://viacep.com.br/ws/${fields.cep}/json/`
+    );
+
+    setFields({
+      ...fields,
+      street: data.logradouro,
+      state: data.uf,
+      city: data.localidade,
+      district: data.bairro,
+    });
+  };
+
   return (
     <Container>
-      <DefaultTitle fontSize={20} title="CADASTRAR ENDEREÇO" />
+      <DefaultTitle fontSize={18} title="CADASTRAR ENDEREÇO" />
+      <InputContainer>
+        <InputMask
+          type={"zip-code"}
+          placeholder="Cep"
+          value={fields.cep}
+          onChangeText={(value) =>
+            setFields({ ...fields, cep: value.replace(/[^0-9]/g, "") })
+          }
+          onBlur={() => {
+            handleGetAddress();
+          }}
+          placeholderTextColor="#C0C0C1"
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Rua"
+          placeholderTextColor="#C0C0C1"
+          value={fields.street}
+          onChangeText={(val) => {
+            setFields({ ...fields, street: val });
+          }}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Número"
+          placeholderTextColor="#C0C0C1"
+          keyboardType="number-pad"
+          value={fields.number}
+          onChangeText={(val) => {
+            setFields({ ...fields, number: val });
+          }}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Complemento"
+          placeholderTextColor="#C0C0C1"
+          value={fields.complement}
+          onChangeText={(val) => {
+            setFields({ ...fields, complement: val });
+          }}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Bairro"
+          placeholderTextColor="#C0C0C1"
+          value={fields.district}
+          onChangeText={(val) => {
+            setFields({ ...fields, district: val });
+          }}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Cidade"
+          placeholderTextColor="#C0C0C1"
+          value={fields.city}
+          onChangeText={(val) => {
+            setFields({ ...fields, city: val });
+          }}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Estado"
+          placeholderTextColor="#C0C0C1"
+          maxLength={2}
+          value={fields.state}
+          onChangeText={(val) => {
+            setFields({ ...fields, state: val });
+          }}
+        />
+      </InputContainer>
+      <DefaultButton
+        buttonText="Cadastrar Endereço"
+        buttonHandle={() => {}}
+        buttonType="primary"
+        marginVertical={30}
+      />
     </Container>
   );
 };
--- src/screens/AddAddress/styled.ts
+++ src/screens/AddAddress/styled.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import Constants from "expo-constants";
+import { TextInputMask } from "react-native-masked-text";
 
 const statusBarHeight = Constants.statusBarHeight;
 
@@ -7,4 +8,27 @@ export const Container = styled.ScrollView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.backgroundLight};
   padding-top: ${statusBarHeight}px;
+`;
+
+export const InputContainer = styled.View`
+  width: 90%;
+  height: 50px;
+  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 5px;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding-left: 10px;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+export const InputMask = styled(TextInputMask)`
+  flex: 1;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primaryText};
 `;
