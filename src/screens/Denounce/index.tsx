aaa0d70337d07545aef332af2fcdbdc8d7d0c6d0
victorbtdias <victorbtdias159@hotmail.com>
2023-11-08 20:33:35 -0300

    Utilizando o Profileinfo com o form de denúncia

--- src/screens/Denounce/index.tsx
+++ src/screens/Denounce/index.tsx
@@ -1,12 +1,36 @@
+import { useNavigation } from "@react-navigation/native";
 import DefaultTitle from "../../components/common/DefaultTitle";
 import NavBar from "../../components/common/NavBar";
-import { Container } from "./styled";
+import ProfileInfo from "../../components/common/ProfileInfo";
+import { PropsStack } from "../../routes";
+import { Button, ButtonText, Container, Input, InputContainer } from "./styled";
+import { Alert } from "react-native";
 
 const Denounce = () => {
+  const navigation = useNavigation<PropsStack>();
+
+  const handleDenounce = () => {
+    navigation.goBack();
+
+    Alert.alert(
+      "Muito obrigado pela sua denúncia, iremos verificar e retornar para você o resultado dela."
+    );
+  };
+
   return (
     <>
       <Container>
         <DefaultTitle title="DENUNCIAR" fontSize={20} />
+        <ProfileInfo />
+        <InputContainer>
+          <Input
+            multiline
+            placeholder="Confirme que você deseja denunciar o perfil acima e escreva aqui seus motivos"
+          />
+        </InputContainer>
+        <Button onPress={handleDenounce}>
+          <ButtonText>DENUNCIAR</ButtonText>
+        </Button>
         <NavBar />
       </Container>
     </>
