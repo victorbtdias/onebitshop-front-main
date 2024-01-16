bf59d890316e2f4081c68f883c83c98a0258d404
victorbtdias <victorbtdias159@hotmail.com>
2024-01-16 14:30:57 -0300

    Criando os produtos como rascunho e como norma

--- src/screens/AddProduct/index.tsx
+++ src/screens/AddProduct/index.tsx
@@ -14,6 +14,10 @@ import { ImagePickerAsset } from "expo-image-picker/build/ImagePicker.types";
 import NavBar from "../../components/common/NavBar";
 import addressService from "../../services/addressService";
 import { Address } from "../../entities/User";
+import { Alert } from "react-native";
+import productService from "../../services/productService";
+import { useNavigation } from "@react-navigation/native";
+import { PropsStack } from "../../routes";
 
 const Category = [
   { value: "Eletrônicos" },
@@ -27,18 +31,20 @@ const Category = [
 ];
 
 const AddProduct = () => {
+  const navigation = useNavigation<PropsStack>();
+
+  const [category, setCategory] = useState("");
+  const [addressId, setAddressId] = useState("");
+  const [address, setAddress] = useState([]);
+  const [images, setImages] = useState<ImagePickerAsset[]>([]);
   const [fields, setFields] = useState({
-    title: "",
+    name: "",
     price: "",
     description: "",
     images: [{}],
     category: "",
     addressId: "",
   });
-  const [category, setCategory] = useState("");
-  const [addressId, setAddressId] = useState("");
-  const [address, setAddress] = useState([]);
-  const [images, setImages] = useState<ImagePickerAsset[]>([]);
 
   const handleGetAddress = async () => {
     const res = await addressService.getAddress();
@@ -53,8 +59,34 @@ const AddProduct = () => {
     setAddress(value);
   };
 
-  const handleSubmitProduct = () => {
-    console.log(fields);
+  const handleSubmitProduct = async (post: string) => {
+    if (
+      Object.values(fields).some((value) => !value) ||
+      !fields.images.length
+    ) {
+      Alert.alert("Um dos campos não está preenchido");
+
+      return;
+    }
+
+    const params = {
+      ...fields,
+      images: images.map(({ uri }) => ({
+        filename: uri.substring(uri.lastIndexOf("/") + 1),
+        uri,
+        url: "",
+        type: `image/${uri.split(".").slice(-1).toString()}`,
+      })),
+      published: post,
+    };
+
+    const { status } = await productService.addProduct(params);
+
+    if (status === 201) {
+      Alert.alert("Seu produto foi cadastrado com sucesso!");
+
+      navigation.navigate("Home");
+    }
   };
 
   useEffect(() => {
@@ -64,7 +96,7 @@ const AddProduct = () => {
       category: category,
       addressId: addressId,
     });
-  }, [images, category, address]);
+  }, [images, category, addressId]);
 
   useEffect(() => {
     handleGetAddress();
@@ -77,11 +109,11 @@ const AddProduct = () => {
         <InputContainer>
           <Input
             placeholder="Título"
-            value={fields.title}
+            value={fields.name}
             onChangeText={(val) => {
               setFields({
                 ...fields,
-                title: val,
+                name: val,
               });
             }}
           />
@@ -124,14 +156,18 @@ const AddProduct = () => {
         />
         <DefaultButton
           buttonText="CADASTRAR E PUBLICAR"
-          buttonHandle={handleSubmitProduct}
+          buttonHandle={() => {
+            handleSubmitProduct("true");
+          }}
           buttonType="primary"
           marginVertical={20}
         />
         <Division>ou</Division>
         <DefaultButton
           buttonText="SALVAR COMO RASCUNHO"
-          buttonHandle={handleSubmitProduct}
+          buttonHandle={() => {
+            handleSubmitProduct("false");
+          }}
           buttonType="secondary"
           marginVertical={20}
         />
