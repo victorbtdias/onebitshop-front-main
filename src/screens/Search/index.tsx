d7ac9b08559f1aeadad44707cba23dbcba9ba617
victorbtdias <victorbtdias159@hotmail.com>
2024-01-17 09:25:46 -0300

    criacao serviço de search e adição na tela

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,20 +1,45 @@
-import React from "react";
-import { Container } from "./styled";
+import React, { useEffect, useState } from "react";
+import { Container, NoResult } from "./styled";
 import Header from "../../components/common/Header";
 import NavBar from "../../components/common/NavBar";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { PropsNavigationStack } from "../../routes";
 import ProductList from "../../components/common/ProductList";
+import { Product } from "../../entities/Product";
+import searchService from "../../services/searchService";
+import Loader from "../Loader";
 
 type Props = NativeStackScreenProps<PropsNavigationStack, "Search">;
 
 const Search = ({ route }: Props) => {
+  const [data, setData] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
   const query = route?.params.query;
+
+  const handleSearch = async () => {
+    const { data } = await searchService.getSearchedProducts(query);
+
+    setData(data?.products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [query]);
 
   return (
     <Container>
       <Header />
-      <ProductList />
+      {!loading ? (
+        data.length <= 0 ? (
+          <NoResult>Sua pesquisa não corresponde a nenhum produto</NoResult>
+        ) : (
+          <ProductList handleGetProducts={handleSearch} products={data} />
+        )
+      ) : (
+        <Loader />
+      )}
       <NavBar />
     </Container>
   );
